1ee65813f4e3583b1f1eb7b9622b12503aa97457
jamesL103 <jameskl7337@gmail.com>
2025-01-07 13:35:03 -0500

    Added main class to jar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,3 +41,10 @@ tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
 }
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = application.mainClass;
+    }
+}
+
