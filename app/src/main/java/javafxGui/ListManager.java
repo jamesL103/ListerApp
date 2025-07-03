64786d6a168910d1e7467f208458e953dc827a1a
jamesL103 <jameskl7337@gmail.com>
2025-07-03 15:00:33 -0400

    Create JavaFx list manager

---
+++ app/src/main/java/javafxGui/ListManager.java
@@ -0,0 +1,52 @@
+package javafxGui;
+
+import javafx.collections.ObservableList;
+import listItemStorage.ListEntry;
+import listItemStorage.ListFileReader;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ListManager {
+
+    //track all lists and their elements
+    private final Map<String, ObservableList<ListEntry>> LISTS;
+    public final String LIST_DIR;
+
+    public ListManager() {
+        LISTS = new HashMap<>();
+
+        LIST_DIR = System.getProperty("user.dir") + "/lists/";
+    }
+
+    public void addList(ListPanel list) {
+        LISTS.put(list.NAME, list.getList());
+    }
+
+    //load all lists from file
+    public void loadAll() {
+        for (String name: LISTS.keySet()) {
+            ObservableList<ListEntry> list = LISTS.get(name);
+            File lstFile = new File(LIST_DIR + name);
+            try {
+                ListFileReader read = new ListFileReader(lstFile);
+                list.addAll(read.loadFromFile());
+            } catch (IOException e) {
+                System.err.println("Error: couldn't load list " + name + " from file: " + e.getMessage());
+                try {
+                    lstFile.createNewFile();
+                } catch (IOException ioException){
+                    System.err.println("Error: couldn't create list file + " + name + ": " + e.getMessage());
+                }
+            }
+        }
+    }
+
+
+    public void saveList(String name) {
+
+    }
+
+}
