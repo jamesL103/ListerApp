936cc9a2bd80b9dce5e93c0111ccadf36b5b2ad5
jamesL103 <jameskl7337@gmail.com>
2025-07-06 17:35:08 -0400

    create display interface

---
+++ app/src/main/java/javafxGui/EntryDisplay.java
@@ -0,0 +1,12 @@
+package javafxGui;
+
+import javafx.geometry.Insets;
+
+public interface EntryDisplay {
+
+    int VERTICAL_SPACING = 10;
+    int DESCRIPTION_COLUMNS = 20;
+    int DESCRIPTION_ROWS = 5;
+    Insets PANEL_INSETS = new Insets(0, 10, 10, 10);
+
+}
