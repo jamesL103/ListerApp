c5fc6905969d5dd49be3cf1ea435d918953218c8
jamesL103 <jameskl7337@gmail.com>
2025-07-06 17:34:41 -0400

    create fonts file

---
+++ app/src/main/java/javafxGui/Fonts.java
@@ -0,0 +1,12 @@
+package javafxGui;
+
+import javafx.scene.text.Font;
+
+public class Fonts {
+
+    public final static String FONT_FAMILY = "Segoe UI";
+
+    public static final Font TITLE = new Font(FONT_FAMILY, 30);
+
+
+}
